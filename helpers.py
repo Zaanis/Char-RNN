64ae1b22e3444084fc07748a045e91134fb6812b
Zaanis <69650111+Zaanis@users.noreply.github.com>
2023-06-14 18:08:56 -0700

    Add files via upload

---
+++ helpers.py
@@ -0,0 +1,37 @@
+# https://github.com/spro/char-rnn.pytorch
+
+import unidecode
+import string
+import random
+import time
+import math
+import torch
+import random
+# Reading and un-unicode-encoding data
+
+all_characters = string.printable
+n_characters = len(all_characters)
+
+def read_file(filename):
+    file = unidecode.unidecode(open(filename).read())
+    return file, len(file)
+
+# Turning a string into a tensor
+
+def char_tensor(string):
+    tensor = torch.zeros(len(string)).long()
+    for c in range(len(string)):
+        try:
+            tensor[c] = all_characters.index(string[c])
+        except:
+            tensor[c] = random.randint(0, len(all_characters) - 1) #cases where a character cant be unidecoded
+    return tensor
+
+# Readable time elapsed
+
+def time_since(since):
+    s = time.time() - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
